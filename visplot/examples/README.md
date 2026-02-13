cb8c70edd5935964dd419f5ea504dc6bc3d45e4f
Emanuel Gafton <ega@ing.iac.es>
2026-02-13 10:33:56 +0000

    Added examples of using POST and SESSION

---
+++ visplot/examples/README.md
@@ -0,0 +1,33 @@
+# Examples for Sending Observation Block (OB) Information to Visplot
+
+This directory contains two example files that demonstrate how to send Observation Block (OB)
+information to Visplot, in one of two ways:
+
+   - via a POST request to the Visplot website (or to a local installation),
+   - by setting PHP SESSION parameters for a local installation.
+
+## Files
+
+### 1. `post_request_example.html`
+This file demonstrates how to send OB information to the Visplot website using a
+POST request. It creates a form dynamically in JavaScript, populates it with OB data
+in JSON format, and submits it to https://www.visplot.com/. This approach is useful
+for pre-populating the target input box on the Visplot website with OB information.
+
+#### Usage:
+1. Open the file in a browser.
+2. The script will automatically submit the OB data to the Visplot website,
+   and will redirect you there.
+
+### 2. `session_example.php`
+This file demonstrates how to set OB information as a session parameter for a local
+installation of Visplot. The OB data is stored in the PHP session and the user is
+redirected to the local Visplot installation.
+In this example, it is assumed that you started Visplot in a container according
+to the main README.md file, and that Visplot is running at http://localhost:8888.
+
+#### Usage:
+1. Ensure you have the local PHP server running at http://localhost:8888.
+2. Access the file via the browser (http://localhost:8888/examples/session_example.php).
+4. The OB data will be set in the session and you will be redirected to the
+   Visplot main page, with the pre-populated target input box.
---
+++ visplot/examples/session_example.php
@@ -0,0 +1,197 @@
+<?php 
+
+$object = array(
+    "Telescope" => "NOT",
+    "Date" => "20170316",
+    "nTargets" => 8,
+    "Targets" => array(
+        "target1" => array(
+            "Name" => "PKS_1510-089",
+            "GroupID" => "20170323_1510_05",
+            "BlockID" => "1510",
+            "RA" => "15:12:50.00",
+            "Dec" => "-09:05:59.00",
+            "Epoch" => "2000.00",
+            "PM" => array(
+                "RA" => "0.000",
+                "Dec" => "0.000"
+            ),
+            "ObsTime" => "431",
+            "Instrument" => "ALFOSC",
+            "Mode" => "IMAPOL",
+            "Imtype" => "science",
+            "Weather" => "Thin Clouds",
+            "Moon" => "Any",
+            "Moondist" => "Any",
+            "Seeing" => "9.9",
+            "Proposal" => "54-020",
+            "Constraint" => "1.99",
+            "Type" => "ToO"
+        ),
+        "target2" => array(
+            "Name" => "PKS_1510-089",
+            "GroupID" => "20170325_1510_06",
+            "BlockID" => "1510",
+            "RA" => "15:12:50.00",
+            "Dec" => "-09:05:59.00",
+            "Epoch" => "2000.00",
+            "PM" => array(
+                "RA" => "0.000",
+                "Dec" => "0.000"
+            ),
+            "ObsTime" => "431",
+            "Instrument" => "ALFOSC",
+            "Mode" => "IMAPOL",
+            "Imtype" => "science",
+            "Weather" => "Thin Clouds",
+            "Moon" => "Any",
+            "Moondist" => "Any",
+            "Seeing" => "9.9",
+            "Proposal" => "54-020",
+            "Constraint" => "1.99",
+            "Type" => "ToO"
+        ),
+        "target3" => array(
+            "Name" => "PKS_1510-089",
+            "GroupID" => "20170329_1510_06",
+            "BlockID" => "1510",
+            "RA" => "15:12:50.00",
+            "Dec" => "-09:05:59.00",
+            "Epoch" => "2000.00",
+            "PM" => array(
+                "RA" => "0.000",
+                "Dec" => "0.000"
+            ),
+            "ObsTime" => "431",
+            "Instrument" => "ALFOSC",
+            "Mode" => "IMAPOL",
+            "Imtype" => "science",
+            "Weather" => "Thin Clouds",
+            "Moon" => "Any",
+            "Moondist" => "Any",
+            "Seeing" => "9.9",
+            "Proposal" => "54-020",
+            "Constraint" => "1.99",
+            "Type" => "ToO"
+        ),
+        "target4" => array(
+            "Name" => "OT_546",
+            "GroupID" => "20170323_1727_02",
+            "BlockID" => "ot546",
+            "RA" => "17:28:18.6",
+            "Dec" => "50:13:10.00",
+            "Epoch" => "2000.00",
+            "PM" => array(
+                "RA" => "0.00",
+                "Dec" => "0.00"
+            ),
+            "ObsTime" => "391",
+            "Instrument" => "ALFOSC",
+            "Mode" => "IMAPOL",
+            "Imtype" => "science",
+            "Weather" => "Thin Clouds",
+            "Moon" => "Any",
+            "Moondist" => "Any",
+            "Seeing" => "9.9",
+            "Proposal" => "54-016",
+            "Constraint" => "1.99",
+            "Type" => "Payback"
+        ),
+        "target5" => array(
+            "Name" => "H_1722+119",
+            "GroupID" => "20170323_1722_02",
+            "BlockID" => "1722",
+            "RA" => "17:25:04.3",
+            "Dec" => "11:52:15.00",
+            "Epoch" => "2000.00",
+            "PM" => array(
+                "RA" => "0.00",
+                "Dec" => "0.00"
+            ),
+            "ObsTime" => "351",
+            "Instrument" => "ALFOSC",
+            "Mode" => "IMAPOL",
+            "Imtype" => "science",
+            "Weather" => "Thin Clouds",
+            "Moon" => "Any",
+            "Moondist" => "Any",
+            "Seeing" => "9.9",
+            "Proposal" => "54-016",
+            "Constraint" => "1.99",
+            "Type" => "Payback"
+        ),
+        "target6" => array(
+            "Name" => "OT_546",
+            "GroupID" => "20170315_1727_01",
+            "BlockID" => "ot546",
+            "RA" => "17:28:18.6",
+            "Dec" => "50:13:10.00",
+            "Epoch" => "2000.00",
+            "PM" => array(
+                "RA" => "0.00",
+                "Dec" => "0.00"
+            ),
+            "ObsTime" => "391",
+            "Instrument" => "ALFOSC",
+            "Mode" => "IMAPOL",
+            "Imtype" => "science",
+            "Weather" => "Thin Clouds",
+            "Moon" => "Any",
+            "Moondist" => "Any",
+            "Seeing" => "9.9",
+            "Proposal" => "54-016",
+            "Constraint" => "1.99",
+            "Type" => "Monitor"
+        ),
+        "target7" => array(
+            "Name" => "1ES_1741+196",
+            "GroupID" => "20170315_1741_01",
+            "BlockID" => "1741",
+            "RA" => "17:43:57.8",
+            "Dec" => "19:35:09.00",
+            "Epoch" => "2000.00",
+            "PM" => array(
+                "RA" => "0.00",
+                "Dec" => "0.00"
+            ),
+            "ObsTime" => "351",
+            "Instrument" => "ALFOSC",
+            "Mode" => "IMAPOL",
+            "Imtype" => "science",
+            "Weather" => "Thin Clouds",
+            "Moon" => "Any",
+            "Moondist" => "Any",
+            "Seeing" => "9.9",
+            "Proposal" => "54-016",
+            "Constraint" => "1.99",
+            "Type" => "Monitor"
+        ),
+        "target8" => array(
+            "Name" => "H_1722+119",
+            "GroupID" => "20170315_1722_01",
+            "BlockID" => "1722",
+            "RA" => "17:25:04.3",
+            "Dec" => "11:52:15.00",
+            "Epoch" => "2000.00",
+            "PM" => array(
+                "RA" => "0.00",
+                "Dec" => "0.00"
+            ),
+            "ObsTime" => "351",
+            "Instrument" => "ALFOSC",
+            "Mode" => "IMAPOL",
+            "Imtype" => "science",
+            "Weather" => "Thin Clouds",
+            "Moon" => "Any",
+            "Moondist" => "Any",
+            "Seeing" => "9.9",
+            "Proposal" => "54-016",
+            "Constraint" => "1.99",
+            "Type" => "Monitor"
+        )
+    )
+);
+
+session_start();
+$_SESSION['obinfo'] = json_encode($object);
+header('Location:http://localhost:8888');
